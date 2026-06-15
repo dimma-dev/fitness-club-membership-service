f5fdc751b276ed4ac72339fee23e6a2b494dddb0
Prus Oleksandr <cheepaxxa2003@gmail.com>
2026-06-15 11:41:28 +0300

    add jwt auth

--- src/config/settings.py
+++ src/config/settings.py
@@ -115,7 +115,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # --- DJANGO REST FRAMEWORK ---
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
