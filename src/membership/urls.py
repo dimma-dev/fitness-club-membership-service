f6584b4bef03de3e844238f85cf71c67220c6780
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-06-15 17:03:47 +0300

    added endpoints to urls

---
+++ src/membership/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from membership.views import MembershipViewSet
+
+app_name = "membership"
+router = routers.DefaultRouter()
+router.register("memberships", MembershipViewSet, basename="membership")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
