98ae5ff495365e3ef1431ebc089e87b7b7701759
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-06-16 01:01:59 +0300

    added custom actions - freeze, resume, upgrade

--- src/membership/serializers.py
+++ src/membership/serializers.py
@@ -47,7 +47,17 @@ class FreezeSerializer(serializers.Serializer):
 
     def validate(self, data):
         if data["frozen_from"] >= data["frozen_to"]:
-            raise serializers.ValidationError("The 'to' date must be after the 'from' date.")
+            raise serializers.ValidationError(
+                "The 'to' date must be after the 'from' date."
+            )
         if data["frozen_from"] < date.today():
-            raise serializers.ValidationError("You can't freeze the past.")
+            raise serializers.ValidationError(
+                "You can't freeze the past."
+            )
+        freeze_days = (self.frozen_to - self.frozen_from).days
+        if freeze_days > 30:
+            raise serializers.ValidationError(
+                "Freeze period cannot exceed 30 days."
+            )
         return data
+
