bd26e06cd33ef64b5e3c9782992bb5fffbde0e8f
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-06-15 14:53:18 +0300

    added missed fields to membership model

--- src/membership/models.py
+++ src/membership/models.py
@@ -9,11 +9,12 @@ class Membership(models.Model):
         ACTIVE = "ACTIVE", "Active"
         FROZEN = "FROZEN", "Frozen"
         EXPIRED = "EXPIRED", "Expired"
+        PENDING = "PENDING", "Pending"
 
     status = models.CharField(
         max_length=10,
         choices=Status.choices,
-        default=Status.ACTIVE,
+        default=Status.PENDING,
     )
 
     start_date = models.DateField()
@@ -21,6 +22,7 @@ class Membership(models.Model):
 
     frozen_from = models.DateField(null=True, blank=True)
     frozen_to = models.DateField(null=True, blank=True)
+    is_frozen_used = models.BooleanField(default=False)
 
     price_at_purchase = models.DecimalField(max_digits=10, decimal_places=2)
     auto_renew = models.BooleanField(default=False)
@@ -32,10 +34,16 @@ class Membership(models.Model):
     member = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name="memberships",
+        related_name="user_memberships",
     )
     plan = models.ForeignKey(
         MembershipPlan,
         on_delete=models.PROTECT(),
-        related_name="memberships",
+        related_name="plan_memberships",
     )
+
+    def __str__(self) -> str:
+        return (
+            f"Membership #{self.pk}: {self.member} - "
+            f"{self.plan.name} ({self.status})"
+        )
