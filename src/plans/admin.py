da5e555906d80c3f2ce93cfefa35e8b9af5990d7
Prus Oleksandr <cheepaxxa2003@gmail.com>
2026-06-16 17:41:32 +0300

    Add plans to admin panel

--- src/plans/admin.py
+++ src/plans/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import MembershipPlan
 
-# Register your models here.
+
+@admin.register(MembershipPlan)
+class MembershipPlanAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "code",
+        "duration_days",
+        "price",
+        "tier",
+    )
+    list_filter = ("tier", "duration_days")
+    search_fields = ("name", "code")
+    ordering = ["price"]
+    prepopulated_fields = {"code": ("name",)}
+
+    fieldsets = (
+        ("General Info", {"fields": ("name", "code", "tier")}),
+        ("Pricing & Terms", {"fields": ("duration_days", "price")}),
+    )
