344e4a482df6654d053dce18e1eb20282ee9ecbb
RustamHadoiev24 <gadoeva155@gmail.com>
2026-06-16 08:54:48 +0000

    fix: clean up code style and flake8 issues

--- src/plans/views.py
+++ src/plans/views.py
@@ -2,10 +2,10 @@ from rest_framework import viewsets, filters, permissions
 from .models import MembershipPlan
 from .serializers import MembershipPlanSerializer
 
+
 class MembershipPlanViewSet(viewsets.ModelViewSet):
     queryset = MembershipPlan.objects.all()
     serializer_class = MembershipPlanSerializer
-    
     filter_backends = [filters.SearchFilter]
     search_fields = ['tier']
 
@@ -13,4 +13,3 @@ class MembershipPlanViewSet(viewsets.ModelViewSet):
         if self.action in ['list', 'retrieve']:
             return [permissions.AllowAny()]
         return [permissions.IsAdminUser()]
-        
