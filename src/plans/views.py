2520e2afe6399c8dc76eec087de7b6b0e3c8686f
Prus Oleksandr <cheepaxxa2003@gmail.com>
2026-06-16 15:31:31 +0300

    Fix filters in MembershipPlanViewSet

--- src/plans/views.py
+++ src/plans/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, filters, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions, viewsets
 from .models import MembershipPlan
 from .serializers import MembershipPlanSerializer
 
@@ -6,8 +7,9 @@ from .serializers import MembershipPlanSerializer
 class MembershipPlanViewSet(viewsets.ModelViewSet):
     queryset = MembershipPlan.objects.all()
     serializer_class = MembershipPlanSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ["tier"]
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["tier"]
 
     def get_permissions(self):
         if self.action in ["list", "retrieve"]:
